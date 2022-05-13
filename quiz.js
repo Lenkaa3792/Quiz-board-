4d6506fc2af36d17a7ca805b9ea74b746758f7a0
Lenkaa3792 <allanlenkaa@gmail.com>
2022-05-13 23:39:51 +0300

    this committ adds lines of code to the js file

--- quiz.js
+++ quiz.js
@@ -1,6 +1,14 @@
 function check(){
     var c=0;
     var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
     if(q1=="lilongwe") {c++}
+    if(q2=="Africa") {c++}
+    if(q3=="Tarmac roads") {c++}
+    if(q4=="Everest") {c++}
+    if(q5=="R.Nile") {c++}
     document.write(c);
 }
