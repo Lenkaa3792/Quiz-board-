84153f03c06632f922618a295e7f1c270566d91b
Lenkaa3792 <allanlenkaa@gmail.com>
2022-05-14 12:38:20 +0300

    this commit changes the last paragraph ID

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function check(){
     var q3=document.quiz.question3.value;
     var q4=document.quiz.question4.value;
     var q5=document.quiz.question5.value;
-    var output=document.getElementById("output");
+    var total=document.getElementById("output");
     var quiz=document.getElementById("quiz");
     if(q1=="lilongwe") {c++}
     if(q2=="Africa") {c++}
@@ -13,6 +13,6 @@ function check(){
     if(q4=="Everest") {c++}
     if(q5=="R.Nile") {c++}
     quiz.style.display="none";
-    output.textContent=${c};
+    total.textContent='${c}';
 
 }
