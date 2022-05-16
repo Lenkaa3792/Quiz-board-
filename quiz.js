64b45e9dbf310448422a46002b88ba26cd2f5456
Lenkaa3792 <allanlenkaa@gmail.com>
2022-05-16 08:45:19 +0300

    remarks

--- quiz.js
+++ quiz.js
@@ -12,11 +12,14 @@ function check(){
     if(q3=="Tarmac roads") {c++}
     if(q4=="Everest") {c++}
     if(q5=="R.Nile") {c++}
-  if (c<=3){
-      results.innerText = c+' :'+'your score is too low'
+  if (c<2){
+      results.innerText = c+' :'+'Poor results,retake the quiz'
   }
-  else{
-    results.innerText = c+' :'+'Good work'
+  else if(c>3){
+    results.innerText = c+' :'+'Excellently passed'
+}else
+{results.innerText = c+' :'+'fairly passed'
+
 }
 
     //document.write(c);
